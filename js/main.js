961c07f6e3ce999b4fefd2289a67cc4b556cc2f4
JennyBambu <jakirokivikvik@gmail.com>
2021-02-27 19:37:38 +0300

    Исправила прошлое дз

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
  * @return {number|null} — случайное число
  */
 
-const RANDOMNUMBER = (min, max, n) => {
+const randomnumber = (min, max, n) => {
   if (min < 0 || max < 0) {
     return null;
   }//неверный диапазон (так как по заданию диапазон может быть только >= 0)
@@ -31,15 +31,15 @@ const CHECKING_OUT = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
 
-let cardsCount = 10;
+const CARDS_COUNT = 10;
 
 /**
  * Функция получения случайного элемента из массива строк
  * @param {array} array — исходный массив строк
  * @return {string} — случайная строка
  */
-const GET_RANDOM_ELEMENT_FROM_ARRAY = (array) => {
-  return array[RANDOMNUMBER(0, array.length - 1)];
+const getRandomElementFromArray = (array) => {
+  return array[randomnumber(0, array.length - 1)];
 }
 
 /**
@@ -48,9 +48,9 @@ const GET_RANDOM_ELEMENT_FROM_ARRAY = (array) => {
  * @param {number} arrayCount — длина случайного массива
  * @return {array} — полученный массив случайной длины
  */
-const MAKE_RANDOM_ARRAY = (array) => {
+const makeRandomArray = (array) => {
   // генерация случайной длины массива
-  const arrayCount = RANDOMNUMBER(1, array.length);
+  const arrayCount = randomnumber(1, array.length);
 
   // обрезка исходный длины до высчитанной случайной длины
   let randomArray = array.slice(0, arrayCount);
@@ -61,23 +61,23 @@ const MAKE_RANDOM_ARRAY = (array) => {
  * Функция генерации карточки объявления
  * @return {object} — карточка объявления
  */
-const CREATECARD = () => {
-  let xRandomValue = RANDOMNUMBER(35.65000, 35.70000, 5);
-  let yRandomValue = RANDOMNUMBER(139.70000, 139.80000, 5);
+const createCard = () => {
+  let xRandomValue = randomnumber(35.65000, 35.70000, 5);
+  let yRandomValue = randomnumber(139.70000, 139.80000, 5);
   return {
-    avatar: `img/avatars/user0${RANDOMNUMBER(0, 8)}.png`,
+    avatar: `img/avatars/user0${randomnumber(0, 8)}.png`,
     offer: {
       title: 'Отличный вариант!',
       address: `${xRandomValue}, ${yRandomValue}`,
-      price: RANDOMNUMBER(200, 2000),
-      type: GET_RANDOM_ELEMENT_FROM_ARRAY(TYPE),
-      rooms: RANDOMNUMBER(1, 10),
-      guests: RANDOMNUMBER(1, 20),
-      checkin: GET_RANDOM_ELEMENT_FROM_ARRAY(CHECKING_OUT),
-      checkout: GET_RANDOM_ELEMENT_FROM_ARRAY(CHECKING_OUT),
-      features: MAKE_RANDOM_ARRAY(FEATURES),
+      price: randomnumber(200, 2000),
+      type: getRandomElementFromArray(TYPE),
+      rooms: randomnumber(1, 10),
+      guests: randomnumber(1, 20),
+      checkin: getRandomElementFromArray(CHECKING_OUT),
+      checkout: getRandomElementFromArray(CHECKING_OUT),
+      features: makeRandomArray(FEATURES),
       description: 'Комфортное жильё',
-      photos: MAKE_RANDOM_ARRAY(PHOTOS),
+      photos: makeRandomArray(PHOTOS),
     },
     location: {
       x: xRandomValue,
@@ -90,9 +90,9 @@ const CREATECARD = () => {
  * @param {number} cardsCount — количество карточек в массиве
  * @return {array} — тестовый массив карточек объявлений
  */
-const MAKECARDS = ()=> {
-  const CARDS = new Array(cardsCount).fill(null).map(() => CREATECARD());
-  return CARDS;
+const makeCards = () => {
+  const cards = new Array(CARDS_COUNT).fill(null).map(() => createCard());
+  return cards;
 }
 
-MAKECARDS(cardsCount);
+makeCards();
