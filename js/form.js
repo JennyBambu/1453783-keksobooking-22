f9ddf428534a7390fb0f3ff56fb3e78a6422007f
JennyBambu <jakirokivikvik@gmail.com>
2021-03-25 19:52:05 +0300

    Сделала 5-2

--- js/form.js
+++ js/form.js
@@ -2,7 +2,12 @@ const form = document.querySelector('.ad-form');
 const formInteractivElements = form.querySelectorAll('fieldset');
 const mapFilter = document.querySelector('.map__filters');
 const mapFilterInteractiveElements = mapFilter.querySelectorAll('fieldset, select');
-
+const PriceMinOnNight = {
+  BUNGALOW: 0,
+  FLAT: 1000,
+  HOUSE: 5000,
+  PALACE: 10000,
+}
 
 const formStatus = (that, thatclass, active, collection) => {
   if(active) {
@@ -18,5 +23,28 @@ const formStatus = (that, thatclass, active, collection) => {
       element.removeAttribute('disabled', 'disabled');
     }});
 }
+
+const selectType = document.querySelector('#type');
+const inputPrice = document.querySelector('#price');
+/**
+ *  Функция настройки зависимости цены за ночь от выбранного типа жилья
+ */
+selectType.addEventListener('change', (evt) => {
+  inputPrice.placeholder = PriceMinOnNight[evt.target.value.toUpperCase()];
+  inputPrice.setAttribute('min', PriceMinOnNight[evt.target.value.toUpperCase()]);
+})
+/**
+ *  Синхронизация времени заезда и выезда
+ */
+const selectTimeIn = document.querySelector('#timein');
+const selectTimeOut = document.querySelector('#timeout');
+
+selectTimeIn.addEventListener('change', (evt) => {
+  selectTimeOut.value = evt.target.value;
+})
+
+selectTimeOut.addEventListener('change', (evt) => {
+  selectTimeIn.value = evt.target.value;
+})
 
 export {formStatus, form, formInteractivElements, mapFilter, mapFilterInteractiveElements};
